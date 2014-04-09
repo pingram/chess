067516a109a58f51b9ed23d0f83c1a05ecef2cf8
Philip Ingram <neodestro734@yahoo.com>
2014-04-09 11:23:06 -0700

    game#move and basic play functionality

---
+++ chess.rb
@@ -0,0 +1,90 @@
+require './board.rb'
+require './pieces.rb'
+
+class Game
+  def initialize
+    @board = Board.new
+    @w_player = HumanPlayer.new(:white)
+    @b_player = HumanPlayer.new(:black)
+  end
+
+  def play
+    puts "\n\nWelcome to Chess - Enterprise Edition!! (with raving)"
+    player = @b_player
+    loop do
+
+      player = @w_player == player ? @b_player : @w_player
+      p player.color
+
+      begin
+        new_move = player.get_move
+      rescue ArgumentError => e
+        puts e
+        retry
+      end
+
+      # p new_move
+
+    end
+  end
+end
+
+class HumanPlayer
+  attr_reader :color
+  def initialize(color)
+    @color = color
+  end
+
+  def get_move
+    puts "Please enter your move:"
+    user_input = gets.chomp.downcase
+
+    if user_input.length != 5
+      raise ArgumentError.new('Must be in format "e2 e4"')
+    end
+
+    # TODO more validating user input
+    move = user_input.split(' ')
+
+    # reversing user input becuase of how 2d arrays work
+    from = move[0].reverse
+    to = move[1].reverse
+
+    player_map = { 'a' => 0,
+                   'b' => 1,
+                   'c' => 2,
+                   'd' => 3,
+                   'e' => 4,
+                   'f' => 5,
+                   'g' => 6,
+                   'h' => 7 }
+
+    from_x, from_y = from.split('')
+    to_x, to_y = to.split('')
+
+    new_from = [from_x.to_i - 1, player_map[from_y]]
+    new_to = [to_x.to_i - 1, player_map[to_y]]
+    # p [new_from, new_to]
+
+    [new_from, new_to]
+  end
+end
+
+
+g = Game.new
+g.play
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
